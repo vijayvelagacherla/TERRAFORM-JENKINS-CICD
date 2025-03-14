96a92ec5ead4a59376ab25ba80c45a9f976f7cc9
vijayvelagacherla <126367336+vijayvelagacherla@users.noreply.github.com>
2025-03-14 17:12:36 +0530

    Update variables.tf
    chnaged

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = ""
 }
 variable "instance_type" {
   description = "instance type for ec2"
