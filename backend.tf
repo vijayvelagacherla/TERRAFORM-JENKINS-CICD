4c3ef3378e70c218070b67f0be54aebc2900cc77
vijayvelagacherla <126367336+vijayvelagacherla@users.noreply.github.com>
2025-03-14 17:07:58 +0530

    Update backend.tf
    chnaged bucketname

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "arha"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
